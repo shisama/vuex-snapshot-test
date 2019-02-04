c215e57b68db1b2355dc5abd129b92c869056518
Masashi Hirano <cherrydog07@gmail.com>
2019-02-04 23:50:38 +0900

    Add validation of mutation as function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const diff = require('snapshot-diff');
 
 module.exports = (state, mutations) => {
-  for (const mutation in mutations) {
-    const name = `${mutation}`;
-    test(name, () => {
+  for (const key of Object.keys(mutations)) {
+    test(key, () => {
+      const mutation = mutations[key];
+      if (typeof mutation !== 'function') {
+        throw new Error(`${key} is not a function.`);
+      }
       const initialState = Object.assign({}, state);
-      const mutatedState = Object.assign({}, state);
-      mutations[name](mutatedState);
-      expect(diff(initialState, mutatedState)).toMatchSnapshot();
+      const mutableState = Object.assign({}, state);
+      mutation(mutableState);
+      expect(diff(initialState, mutableState)).toMatchSnapshot();
     })
   }
 }
